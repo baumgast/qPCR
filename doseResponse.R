8faae69c902564a4353b9cd31f0b67e93e492224
baumgast <baumsen@gmail.com>
2012-07-30 17:04:21 +0200

    initialized file to read in and plot gene expression data

---
+++ doseResponse.R
@@ -0,0 +1,5 @@
+#dose response qPCR data analysis
+
+setwd('~/Dropbox/qPCR results/dose response/dense sampling/')
+a = read.csv('2012-07-24-dense-dose-response-new-TFF1-primer-Results.csv', header = T)
+
